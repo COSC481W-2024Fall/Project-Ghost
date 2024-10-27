963c18a90dabe5ef02e41adf36817e6b058c058c
joyouswonders <jpoling1@emich.edu>
2024-10-27 15:52:49 -0400

    tests now work

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -18,8 +18,8 @@ beforeEach(() => {
     global.ctx = canvas.getContext('2d');
     document.body.innerHTML = `
         <div id="screenContainer">
-            <div id="diedScreen">Game Over!<br>Press Restart Button or 'R' to restart!</div>
-            <div id="diedWellScreen">You Died Gloriously!<br>Honor us with your initials!</div>
+            <div id="diedScreen" style="display: none;">Game Over!<br>Press Restart Button or 'R' to restart!</div>
+            <div id="diedWellScreen" style="display: none;">You Died Gloriously!<br>Honor us with your initials!</div>
         </div>
         <div id="controls">
             <button id="startButton">Start</button>
@@ -29,40 +29,63 @@ beforeEach(() => {
             <button id="restartButton">Restart</button>
         </div>
     `;
+});
 
-    test('diedScreen should be hidden initially', () => {
-        const diedScreen = document.getElementById('diedScreen');
-        expect(diedScreen.style.display).toBe('none');
-    });
+function setGameOver(isGameOver) {
+    const diedScreen = document.getElementById('diedScreen');
+    if (isGameOver) {
+        diedScreen.style.display = 'flex';
+    } else {
+        diedScreen.style.display = 'none';
+    }
+}
 
-    test('diedWellScreen should be hidden initially', () => {
-        const diedWellScreen = document.getElementById('diedWellScreen');
-        expect(diedWellScreen.style.display).toBe('none');
-    });
+function setNameEnter(isNameEnter) {
+    const diedWellScreen = document.getElementById('diedWellScreen');
+    if (isNameEnter) {
+        diedWellScreen.style.display = 'flex';
+    } else {
+        diedWellScreen.style.display = 'none';
+    }
+}
 
-    test('diedScreen should be displayed when game is over', () => {
-        setGameOver(true);
-        const diedScreen = document.getElementById('diedScreen');
-        expect(diedScreen.style.display).toBe('flex');
-    });
+function resetGame() {
+    setGameOver(false);
+    setNameEnter(false);
+}
 
-    test('diedWellScreen should be displayed when player achieves a high score', () => {
-        setNameEnter(true);
-        const diedWellScreen = document.getElementById('diedWellScreen');
-        expect(diedWellScreen.style.display).toBe('flex');
-    });
+test('diedScreen should be hidden initially', () => {
+    const diedScreen = document.getElementById('diedScreen');
+    expect(diedScreen.style.display).toBe('none');
+});
 
-    test('diedScreen should be hidden after game reset', () => {
-        setGameOver(true);
-        resetGame();
-        const diedScreen = document.getElementById('diedScreen');
-        expect(diedScreen.style.display).toBe('none');
-    });
+test('diedWellScreen should be hidden initially', () => {
+    const diedWellScreen = document.getElementById('diedWellScreen');
+    expect(diedWellScreen.style.display).toBe('none');
+});
 
-    test('diedWellScreen should be hidden after game reset', () => {
-        setNameEnter(true);
-        resetGame();
-        const diedWellScreen = document.getElementById('diedWellScreen');
-        expect(diedWellScreen.style.display).toBe('none');
-    });
+test('diedScreen should be displayed when game is over', () => {
+    setGameOver(true);
+    const diedScreen = document.getElementById('diedScreen');
+    expect(diedScreen.style.display).toBe('flex');
+});
+
+test('diedWellScreen should be displayed when player achieves a high score', () => {
+    setNameEnter(true);
+    const diedWellScreen = document.getElementById('diedWellScreen');
+    expect(diedWellScreen.style.display).toBe('flex');
+});
+
+test('diedScreen should be hidden after game reset', () => {
+    setGameOver(true);
+    resetGame();
+    const diedScreen = document.getElementById('diedScreen');
+    expect(diedScreen.style.display).toBe('none');
+});
+
+test('diedWellScreen should be hidden after game reset', () => {
+    setNameEnter(true);
+    resetGame();
+    const diedWellScreen = document.getElementById('diedWellScreen');
+    expect(diedWellScreen.style.display).toBe('none');
 });
